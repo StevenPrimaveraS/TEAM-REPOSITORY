c929b2d1c79fbbaeda373e7dab52bae9e2a82747
Dominic Leroux <201423084@collegeahuntsic.qc.ca>
2016-09-19 17:22:02 -0400

    Petites modifs

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/util/BDCreateur.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/util/BDCreateur.java
@@ -3,11 +3,10 @@ package ca.qc.collegeahuntsic.bibliotheque.util;
 
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 
 /**
- * <pre>
+ *
  *
  *Permet de cr�er la BD utilis�e par Biblio.java.
  *
@@ -15,56 +14,71 @@ import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
  *           1- bd nom de la BD
  *           2- user id pour �tablir une connexion avec le serveur SQL
  *           3- mot de passe pour le user id
- * </pre>
  *
- * @author Math -
+ *
+ * @author Mathieu
  */
 class BDCreateur {
-	/**
-	 * @author Math -
-	 * @throws SQLException
-	 *             -
-	 * @param args
-	 *            -
-	 * @throws Exception
-	 *             -
-	 */
-	public static void main(String args[]) throws Exception, SQLException {
+    /**
+     * Methode main.
+     * @param args
+     * @throws Exception
+     * @throws SQLException
+     * @author Mathieu
+     */
+    public static void main(String args[]) throws Exception,
+        SQLException {
 
-		if (args.length < 3) {
-			System.out.println("Usage: java CreerBD <serveur> <bd> <user> <password>");
-			return;
-		}
+        if(args.length < 3) {
+            System.out.println("Usage: java CreerBD <serveur> <bd> <user> <password>");
+            return;
+        }
 
-		Connexion cx = new Connexion(args[0], args[1], args[2], args[3]);
+        Connexion cx = new Connexion(args[0],
+            args[1],
+            args[2],
+            args[3]);
 
-		Statement stmt = cx.getConnection().createStatement();
+        Statement stmt = cx.getConnection().createStatement();
 
-		stmt.executeUpdate("DROP TABLE membre CASCADE CONSTRAINTS");
-		stmt.executeUpdate("CREATE TABLE membre ( " + "idMembre        number(3) check(idMembre > 0), "
-				+ "nom             varchar(10) NOT NULL, " + "telephone       number(10) , "
-				+ "limitePret      number(2) check(limitePret > 0 and limitePret <= 10) , "
-				+ "nbpret          number(2) default 0 check(nbpret >= 0) , "
-				+ "CONSTRAINT cleMembre PRIMARY KEY (idMembre), "
-				+ "CONSTRAINT limiteNbPret check(nbpret <= limitePret) " + ")");
+        stmt.executeUpdate("DROP TABLE membre CASCADE CONSTRAINTS");
+        stmt.executeUpdate("CREATE TABLE membre ( "
+            + "idMembre        number(3) check(idMembre > 0), "
+            + "nom             varchar(10) NOT NULL, "
+            + "telephone       number(10) , "
+            + "limitePret      number(2) check(limitePret > 0 and limitePret <= 10) , "
+            + "nbpret          number(2) default 0 check(nbpret >= 0) , "
+            + "CONSTRAINT cleMembre PRIMARY KEY (idMembre), "
+            + "CONSTRAINT limiteNbPret check(nbpret <= limitePret) "
+            + ")");
 
-		stmt.executeUpdate("DROP TABLE livre CASCADE CONSTRAINTS");
-		stmt.executeUpdate("CREATE TABLE livre ( " + "idLivre         number(3) check(idLivre > 0) , "
-				+ "titre           varchar(10) NOT NULL, " + "auteur          varchar(10) NOT NULL, "
-				+ "dateAcquisition date not null, " + "idMembre        number(3) , " + "datePret        date , "
-				+ "CONSTRAINT cleLivre PRIMARY KEY (idLivre), "
-				+ "CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre " + ")");
+        stmt.executeUpdate("DROP TABLE livre CASCADE CONSTRAINTS");
+        stmt.executeUpdate("CREATE TABLE livre ( "
+            + "idLivre         number(3) check(idLivre > 0) , "
+            + "titre           varchar(10) NOT NULL, "
+            + "auteur          varchar(10) NOT NULL, "
+            + "dateAcquisition date not null, "
+            + "idMembre        number(3) , "
+            + "datePret        date , "
+            + "CONSTRAINT cleLivre PRIMARY KEY (idLivre), "
+            + "CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre "
+            + ")");
 
-		stmt.executeUpdate("DROP TABLE reservation CASCADE CONSTRAINTS");
-		stmt.executeUpdate("CREATE TABLE reservation ( " + "idReservation   number(3) , "
-				+ "idMembre        number(3) , " + "idLivre         number(3) , " + "dateReservation date , "
-				+ "CONSTRAINT cleReservation PRIMARY KEY (idReservation) , "
-				+ "CONSTRAINT cleCandidateReservation UNIQUE (idMembre,idLivre) , "
-				+ "CONSTRAINT refReservationMembre FOREIGN KEY (idMembre) REFERENCES membre " + "  ON DELETE CASCADE , "
-				+ "CONSTRAINT refReservationLivre FOREIGN KEY (idLivre) REFERENCES livre " + "  ON DELETE CASCADE "
-				+ ")");
+        stmt.executeUpdate("DROP TABLE reservation CASCADE CONSTRAINTS");
+        stmt.executeUpdate("CREATE TABLE reservation ( "
+            + "idReservation   number(3) , "
+            + "idMembre        number(3) , "
+            + "idLivre         number(3) , "
+            + "dateReservation date , "
+            + "CONSTRAINT cleReservation PRIMARY KEY (idReservation) , "
+            + "CONSTRAINT cleCandidateReservation UNIQUE (idMembre,idLivre) , "
+            + "CONSTRAINT refReservationMembre FOREIGN KEY (idMembre) REFERENCES membre "
+            + "  ON DELETE CASCADE , "
+            + "CONSTRAINT refReservationLivre FOREIGN KEY (idLivre) REFERENCES livre "
+            + "  ON DELETE CASCADE "
+            + ")");
 
-		stmt.close();
-		cx.fermer();
-	}
+        stmt.close();
+        cx.fermer();
+    }
 }
--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/util/GestionBibliotheque.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/util/GestionBibliotheque.java
@@ -2,7 +2,6 @@
 package ca.qc.collegeahuntsic.bibliotheque.util;
 
 import java.sql.SQLException;
-
 import ca.qc.collegeahuntsic.bibliotheque.dao.LivreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.MembreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.ReservationDAO;
@@ -33,57 +32,68 @@ import ca.qc.collegeahuntsic.bibliotheque.service.ReservationService;
  * @author Math -
  */
 public class GestionBibliotheque {
-	public Connexion cx;
+    public Connexion cx;
 
-	public LivreDAO livre;
+    public LivreDAO livre;
 
-	public MembreDAO membre;
+    public MembreDAO membre;
 
-	public ReservationDAO reservation;
+    public ReservationDAO reservation;
 
-	public LivreService gestionLivre;
+    public LivreService gestionLivre;
 
-	public MembreService gestionMembre;
+    public MembreService gestionMembre;
 
-	public PretService gestionPret;
+    public PretService gestionPret;
 
-	public ReservationService gestionReservation;
+    public ReservationService gestionReservation;
 
-	public GestionInterrogation gestionInterrogation;
+    public GestionInterrogation gestionInterrogation;
 
-	/**
-	 * Ouvre une connexion avec la BD relationnelle et alloue les gestionnaires
-	 * de transactions et de tables.
-	 *
-	 * <pre>
-	 *
-	 * &#64;param serveur SQL
-	 * &#64;param bd nom de la bade de donn�es
-	 * &#64;param user user id pour �tablir une connexion avec le serveur SQL
-	 * &#64;param password mot de passe pour le user id
-	 * &#64;throws BibliothequeException
-	 * &#64;throws SQLException
-	 * </pre>
-	 */
-	public GestionBibliotheque(String serveur, String bd, String user, String password)
-			throws BibliothequeException, SQLException {
-		// allocation des objets pour le traitement des transactions
-		this.cx = new Connexion(serveur, bd, user, password);
-		this.livre = new LivreDAO(this.cx);
-		this.membre = new MembreDAO(this.cx);
-		this.reservation = new ReservationDAO(this.cx);
-		this.gestionLivre = new LivreService(this.livre, this.reservation);
-		this.gestionMembre = new MembreService(this.membre, this.reservation);
-		this.gestionPret = new PretService(this.livre, this.membre, this.reservation);
-		this.gestionReservation = new ReservationService(this.livre, this.membre, this.reservation);
-		this.gestionInterrogation = new GestionInterrogation(this.cx);
-	}
+    /**
+     * Ouvre une connexion avec la BD relationnelle et alloue les gestionnaires
+     * de transactions et de tables.
+     *
+     *
+     *
+     * @param serveur SQL
+     * @param bd nom de la bade de données
+     * @param user user id pour établir une connexion avec le serveur SQL
+     * @param password mot de passe pour le user id
+     * @throws BibliothequeException
+     * @throws SQLException
+     */
+    public GestionBibliotheque(String serveur,
+        String bd,
+        String user,
+        String password) throws BibliothequeException,
+        SQLException {
+        // allocation des objets pour le traitement des transactions
+        this.cx = new Connexion(serveur,
+            bd,
+            user,
+            password);
+        this.livre = new LivreDAO(this.cx);
+        this.membre = new MembreDAO(this.cx);
+        this.reservation = new ReservationDAO(this.cx);
+        this.gestionLivre = new LivreService(this.livre,
+            this.reservation);
+        this.gestionMembre = new MembreService(this.membre,
+            this.reservation);
+        this.gestionPret = new PretService(this.livre,
+            this.membre,
+            this.reservation);
+        this.gestionReservation = new ReservationService(this.livre,
+            this.membre,
+            this.reservation);
+        this.gestionInterrogation = new GestionInterrogation(this.cx);
+    }
 
-	/**
-	 * Fermeture de la connexion
-	 */
-	public void fermer() throws SQLException {
-		// fermeture de la connexion
-		this.cx.fermer();
-	}
+    /**
+     * Fermeture de la connexion
+     */
+    public void fermer() throws SQLException {
+        // fermeture de la connexion
+        this.cx.fermer();
+    }
 }
