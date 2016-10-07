e4e44a5b8455b50b2ba94466439a725cc2ed3e1c
MathieuLafond <201475492@collegeahuntsic.qc.ca>
2016-10-07 10:30:00 -0400

    correction javadoc package info de Service

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/package-info.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/package-info.java
@@ -1,7 +1,7 @@
 // Fichier package-info.java
 // Auteur : Primavera Sequeira Steven
 // Date de création : 2016-09-12
-/**.
+/**
  * Provides the Service classes used with the bibliothèque.
  *
  * @author Primavera Sequeira Steven
