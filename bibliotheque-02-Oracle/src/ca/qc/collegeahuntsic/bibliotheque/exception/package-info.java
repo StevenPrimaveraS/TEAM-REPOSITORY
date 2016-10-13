1789c1ac58db27fce2e0d70053fff567bc15c568
Steven Primavera Sequeira <Steven.Primavera-Sequeira.1@collegeahuntsic.qc.ca>
2016-10-13 12:20:20 -0400

    Bibliotheque 02 - Oracle. Version Initiale.

---
+++ bibliotheque-02-Oracle/src/ca/qc/collegeahuntsic/bibliotheque/exception/package-info.java
@@ -0,0 +1,10 @@
+// Fichier package-info.java
+// Auteur : Anthony Chan
+// Date de création : 2016-09-12
+/**
+ * Provides the exception classes used with the bibliothèque.
+ *
+ * @author Anthony Chan
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque.exception;
---
+++ bibliotheque-02-Oracle/src/ca/qc/collegeahuntsic/bibliotheque/facade/Facade.java
@@ -0,0 +1,24 @@
+// Fichier Facade.java
+// Auteur : Primavera Sequeira Steven
+// Date de création : 2016-09-14
+
+package ca.qc.collegeahuntsic.bibliotheque.facade;
+
+import java.io.Serializable;
+
+/**.
+ * Classe de base pour toutes les façades.
+ *
+ * @author Primavera Sequeira Steven
+ */
+public class Facade implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructeur par défaut.
+     */
+    public Facade() {
+
+        super();
+    }
+}
