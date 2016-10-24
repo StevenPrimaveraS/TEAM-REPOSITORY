a257747627dba72f4b0b91a734b9f715216b5d4d
Steven Primavera Sequeira <Steven.Primavera-Sequeira.1@collegeahuntsic.qc.ca>
2016-10-24 16:49:03 -0400

    Bibliotheque 04 -Initial Version.

---
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/package-info.java
@@ -0,0 +1,10 @@
+// Fichier package-info.java
+// Auteur : Primavera Sequeira Steven
+// Date de creation : 2016-09-12
+/**
+ * Provides the DAO classes used with the bibliothèque.
+ *
+ * @author Mathieu Lafond
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque.dao;
