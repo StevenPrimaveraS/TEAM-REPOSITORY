aa7dc3f6aa5befc94db343d6a7d35dacec338b5c
Anthony Chan <Anthony.Chan.1@collegeahuntsic.qc.ca>
2016-11-03 13:57:59 -0400

    bibliotheque-05 membreDAO de fait

--- bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
+++ bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
@@ -5,12 +5,13 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
 
 import java.util.List;
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionValueException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import org.hibernate.Session;
 
 /**
  * Interface DAO pour manipuler les membres dans la base de données.
@@ -22,20 +23,22 @@ public interface IMembreDAO extends IDAO {
      * Trouve les livres à partir d'un titre. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun livre
      * n'est trouvé, une {@link List} vide est retournée.
      *
-     * @param connexion La connexion à utiliser
+     * @param session La session à utiliser
      * @param nom le nom du membre a trouver.
      * @param sortByPropertyName Le nom de la propriété à utiliser pour classer
      * @return La liste des livres correspondants ; une liste vide sinon
      * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
      * @throws InvalidCriterionException Si le titre est <code>null</code>
+     * @throws InvalidCriterionValueException Si la value est <code>null</code>
      * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
      * @throws DAOException S'il y a une erreur avec la base de données
      */
-    List<MembreDTO> findByNom(Connexion connexion,
+    List<MembreDTO> findByNom(Session session,
         String nom,
         String sortByPropertyName) throws InvalidHibernateSessionException,
         InvalidCriterionException,
         InvalidSortByPropertyException,
-        DAOException;
+        DAOException,
+        InvalidCriterionValueException;
 
 }
