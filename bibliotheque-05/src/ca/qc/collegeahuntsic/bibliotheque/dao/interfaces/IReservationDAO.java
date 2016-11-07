cea4ef528779547eb24330330a19f68316e7a7ec
Anthony Chan <Anthony.Chan.1@collegeahuntsic.qc.ca>
2016-11-07 09:12:45 -0500

    Bibliotheque-05 reservationDAO de fait

--- bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IReservationDAO.java
+++ bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IReservationDAO.java
@@ -5,12 +5,13 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
 
 import java.util.List;
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.ReservationDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionValueException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import org.hibernate.classic.Session;
 
 /**
  * TODO Auto-generated class javadoc.
@@ -22,19 +23,21 @@ public interface IReservationDAO extends IDAO {
      * Trouve les livres à partir d'un titre. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun livre
      * n'est trouvé, une {@link List} vide est retournée.
      *
-     * @param connexion La connexion à utiliser
+     * @param session La session à utiliser
      * @param idLivre ID du livre à trouver
      * @param sortByPropertyName Le nom de la propriété à utiliser pour classer
      * @return La liste des livres correspondants ; une liste vide sinon
      * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
      * @throws InvalidCriterionException Si le titre est <code>null</code>
+     * @throws InvalidCriterionValueException Si la value est <code>null</code>
      * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
      * @throws DAOException S'il y a une erreur avec la base de données
      */
-    List<ReservationDTO> findByLivre(Connexion connexion,
+    List<ReservationDTO> findByLivre(Session session,
         String idLivre,
         String sortByPropertyName) throws InvalidHibernateSessionException,
         InvalidCriterionException,
+        InvalidCriterionValueException,
         InvalidSortByPropertyException,
         DAOException;
 
@@ -42,19 +45,21 @@ public interface IReservationDAO extends IDAO {
      * Trouve les livres à partir d'un titre. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun livre
      * n'est trouvé, une {@link List} vide est retournée.
      *
-     * @param connexion La connexion à utiliser
+     * @param session La connexion à utiliser
      * @param idMembre ID du membre à trouver
      * @param sortByPropertyName Le nom de la propriété à utiliser pour classer
      * @return La liste des livres correspondants ; une liste vide sinon
      * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
      * @throws InvalidCriterionException Si le titre est <code>null</code>
+     * @throws InvalidCriterionValueException Si la value est <code>null</code>
      * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
      * @throws DAOException S'il y a une erreur avec la base de données
      */
-    List<ReservationDTO> findByMembre(Connexion connexion,
+    List<ReservationDTO> findByMembre(Session session,
         String idMembre,
         String sortByPropertyName) throws InvalidHibernateSessionException,
         InvalidCriterionException,
+        InvalidCriterionValueException,
         InvalidSortByPropertyException,
         DAOException;
 }
