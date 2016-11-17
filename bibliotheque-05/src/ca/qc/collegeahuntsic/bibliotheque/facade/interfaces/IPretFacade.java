816ad99a45afaeb07902feee0205f250ce94ae18
Anthony Chan <Anthony.Chan.1@collegeahuntsic.qc.ca>
2016-11-17 11:17:58 -0500

    Bibliotheque-05 facade du prof

--- bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IPretFacade.java
+++ bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IPretFacade.java
@@ -1,116 +1,76 @@
 // Fichier IPretFacade.java
-// Auteur : Gilles Benichou
-// Date de création : 2016-10-27
+// Auteur : Gilles Bénichou
+// Date de création : 2016-05-18
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.interfaces;
 
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.PretDTO;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.InvalidLoanLimitException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.MissingLoanException;
+import org.hibernate.Session;
 
 /**
- * Interface de façade pour manipuler les prets dans la base de données.
+ * Interface de façade pour manipuler les prêts dans la base de données.
  *
- * @author Gilles Benichou
+ * @author Gilles Bénichou
  */
 public interface IPretFacade extends IFacade {
     /**
      * Commence un prêt.
      *
-     * @param connexion La connexion à utiliser
-     * @param pretDTO Le pret èa commencer
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le livre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
-     * @throws MissingDTOException Si le livre n'existe pas
-     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
-     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
+     * @param session La session Hibernate à utiliser
+     * @param pretDTO Le prêt à commencer
+     * @throws InvalidHibernateSessionException Si la session Hibernate est <code>null</code>
+     * @throws InvalidDTOException Si le prêt est <code>null</code>
      * @throws ExistingLoanException Si le livre a été prêté
-     * @throws InvalidLoanLimitException Si la limite de prêt est excedée
+     * @throws InvalidLoanLimitException Si le membre a atteint sa limite de prêt
      * @throws ExistingReservationException Si le livre a été réservé
      * @throws FacadeException S'il y a une erreur avec la base de données
      */
-    void commencer(Connexion connexion,
+    void commencer(Session session,
         PretDTO pretDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidCriterionException,
-        InvalidSortByPropertyException,
         ExistingLoanException,
         InvalidLoanLimitException,
         ExistingReservationException,
-        InvalidDTOClassException,
         FacadeException;
 
     /**
-     * Renouvelle le pret d'un livre.
+     * Renouvelle le prêt d'un livre.
      *
-     * @param connexion La connexion à utiliser
-     * @param pretDTO Le pret èa commencer
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le livre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
-     * @throws MissingDTOException Si le livre n'existe pas
-     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
-     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
-     * @throws ExistingLoanException Si le livre a été prêté
-     * @throws InvalidLoanLimitException Si la limite de prêt est excedée
+     * @param session La session Hibernate à utiliser
+     * @param pretDTO Le prêt à renouveler
+     * @throws InvalidHibernateSessionException Si la session Hibernate est <code>null</code>
+     * @throws InvalidDTOException Si le prêt est <code>null</code>
+     * @throws MissingLoanException Si le livre n'a pas encore été prêté
      * @throws ExistingReservationException Si le livre a été réservé
      * @throws FacadeException S'il y a une erreur avec la base de données
      */
-    void renouveler(Connexion connexion,
+    void renouveler(Session session,
         PretDTO pretDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidCriterionException,
-        InvalidSortByPropertyException,
-        ExistingLoanException,
-        InvalidLoanLimitException,
+        MissingLoanException,
         ExistingReservationException,
-        InvalidDTOClassException,
         FacadeException;
 
     /**
      * Termine un prêt.
      *
-     * @param connexion La connexion à utiliser
-     * @param pretDTO Le pret èa commencer
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le livre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
-     * @throws MissingDTOException Si le livre n'existe pas
-     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
-     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
-     * @throws ExistingLoanException Si le livre a été prêté
-     * @throws InvalidLoanLimitException Si la limite de prêt est excedée
-     * @throws ExistingReservationException Si le livre a été réservé
+     * @param session La session Hibernate à utiliser
+     * @param pretDTO Le prêt à terminer
+     * @throws InvalidHibernateSessionException Si la session Hibernate est <code>null</code>
+     * @throws InvalidDTOException Si le prêt est <code>null</code>
+     * @throws MissingLoanException Si le livre n'a pas encore été prêté
      * @throws FacadeException S'il y a une erreur avec la base de données
      */
-    void terminer(Connexion connexion,
+    void terminer(Session session,
         PretDTO pretDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidCriterionException,
-        InvalidSortByPropertyException,
-        ExistingLoanException,
-        InvalidLoanLimitException,
-        ExistingReservationException,
-        InvalidDTOClassException,
+        MissingLoanException,
         FacadeException;
 }
--- bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IReservationFacade.java
+++ bibliotheque-05/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IReservationFacade.java
@@ -1,114 +1,76 @@
 // Fichier IReservationFacade.java
-// Auteur : Gilles Benichou
-// Date de création : 2016-10-27
+// Auteur : Gilles Bénichou
+// Date de création : 2016-05-18
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.interfaces;
 
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.ReservationDTO;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.InvalidLoanLimitException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.service.MissingLoanException;
+import org.hibernate.Session;
 
 /**
  * Interface de façade pour manipuler les réservations dans la base de données.
  *
- * @author Gilles Benichou
+ * @author Gilles Bénichou
  */
 public interface IReservationFacade extends IFacade {
     /**
      * Place une réservation.
      *
-     * @param connexion La connexion à utiliser
-     * @param reservationDTO La réservation èa placer
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le livre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
-     * @throws MissingDTOException Si le livre n'existe pas
-     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
-     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
-     * @throws ExistingLoanException Si le livre a été prêté
-     * @throws MissingLoanException Si le pret n'existe pas.
-     * @throws ExistingReservationException Si le livre a été réservé
+     * @param session La session Hibernate à utiliser
+     * @param reservationDTO La réservation à placer
+     * @throws InvalidHibernateSessionException Si la session Hibernate est <code>null</code>
+     * @throws InvalidDTOException Si la réservation est <code>null</code>
+     * @throws MissingLoanException Si le livre n'a pas encore été prêté
+     * @throws ExistingLoanException Si le livre est déjà prêté au membre
+     * @throws ExistingReservationException Si le membre a déjà réservé ce livre
      * @throws FacadeException S'il y a une erreur avec la base de données
      */
-    void placer(Connexion connexion,
+    void placer(Session session,
         ReservationDTO reservationDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidCriterionException,
-        InvalidSortByPropertyException,
         MissingLoanException,
         ExistingLoanException,
         ExistingReservationException,
-        InvalidDTOClassException,
         FacadeException;
 
     /**
      * Utilise une réservation.
      *
-     * @param connexion La connexion à utiliser
-     * @param reservationDTO La réservation èa placer
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le livre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
-     * @throws MissingDTOException Si le livre n'existe pas
-     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
-     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
-     * @throws ExistingLoanException Si le livre a été prêté
-     * @throws MissingLoanException Si le pret n'existe pas.
-     * @throws InvalidLoanLimitException Si la limite de prêt est excedée
-     * @throws ExistingReservationException Si le livre a été réservé
+     * @param session La session Hibernate à utiliser
+     * @param reservationDTO La réservation à utiliser
+     * @throws InvalidHibernateSessionException Si la session Hibernate est <code>null</code>
+     * @throws InvalidDTOException Si la réservation est <code>null</code>
+     * @throws ExistingReservationException Si la réservation n'est pas la première de la liste
+     * @throws ExistingLoanException Si le livre est déjà prêté au membre
+     * @throws InvalidLoanLimitException Si le membre a atteint sa limite de prêt
      * @throws FacadeException S'il y a une erreur avec la base de données
      */
-    void utiliser(Connexion connexion,
+    void utiliser(Session session,
         ReservationDTO reservationDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidCriterionException,
-        InvalidSortByPropertyException,
         ExistingReservationException,
         ExistingLoanException,
         InvalidLoanLimitException,
-        InvalidDTOClassException,
         FacadeException;
 
     /**
-     * Annule une reservation.
+     * Annule une réservation.
      *
-     * @param connexion La connexion à utiliser
-     * @param reservationDTO La réservation èa placer
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidDTOException Si le livre est <code>null</code>
-     * @throws InvalidDTOClassException Si la classe du livre n'est pas celle que prend en charge le DAO
-     * @throws InvalidPrimaryKeyException Si la clef primaire du livre est <code>null</code>
-     * @throws MissingDTOException Si le livre n'existe pas
-     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
-     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
-     * @throws ExistingLoanException Si le livre a été prêté
-     * @throws MissingLoanException Si le pret n'existe pas.
-     * @throws InvalidLoanLimitException Si la limite de prêt est excedée
-     * @throws ExistingReservationException Si le livre a été réservé
+     * @param session La session Hibernate à utiliser
+     * @param reservationDTO Le reservation à annuler
+     * @throws InvalidHibernateSessionException Si la session Hibernate est <code>null</code>
+     * @throws InvalidDTOException Si la réservation est <code>null</code>
      * @throws FacadeException S'il y a une erreur avec la base de données
      */
-    void annuler(Connexion connexion,
+    void annuler(Session session,
         ReservationDTO reservationDTO) throws InvalidHibernateSessionException,
         InvalidDTOException,
-        InvalidPrimaryKeyException,
-        MissingDTOException,
-        InvalidDTOClassException,
         FacadeException;
 }
